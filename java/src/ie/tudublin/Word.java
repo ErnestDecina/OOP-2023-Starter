6b6954c926abcb2b2a203ef2115be4ccbb632a5e
Ernest Decina <ernestjohn007@gmail.com>
2023-04-26 17:58:36 +0100

    Updated Word.java

--- java/src/ie/tudublin/Word.java
+++ java/src/ie/tudublin/Word.java
@@ -3,25 +3,59 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 public class Word {
-    String word;
-    ArrayList<Follow> followArrayList;
- 
-    public Word(String word) {
-        this.word = word;
-        this.followArrayList = new ArrayList<Follow>();
-    } // End Word constructor
+    private String word;
+    private ArrayList<Follow> follows;
 
-
-
-    // Accessor Methods
     public String getWord() {
         return word;
     }
 
-    public ArrayList<Follow> getWordArrayList() {
-        return followArrayList;
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public ArrayList<Follow> getFollows() {
+        return follows;
+    }
+
+    public void setFollows(ArrayList<Follow> follows) {
+        this.follows = follows;
+    }
+    public void addFollow(Follow f)
+    {
+        follows.add(f);
+    }
+    public void addFollowCount(Follow f)
+    {
+        f.setCount(f.getCount()+1);
+    }
+
+    public int findFollow(String word)
+    {
+        for(Follow f:follows)
+        {
+            if(f.getWord().equals(word))
+            {
+                return follows.indexOf(f);
+            }
+        }
+        return -1;
     }
 
 
+    public Word(String word) {
+        this.word = word;
+        follows = new ArrayList<Follow>();
+    }
     
+    public String toString()
+    {
+        String result = "";
+        result += word + ":";
+        for(Follow f:follows)
+        {
+            result += " " + f.toString();
+        }
+        return result;
+    }
 } // End class Word

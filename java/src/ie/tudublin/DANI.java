e7d11c32088eb3e3bc3e6bbcd3975405d7ccefae
Ernest Decina <ernestjohn007@gmail.com>
2023-04-26 16:20:31 +0100

    Made basic setup of DANI.java

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -41,4 +41,29 @@ public class DANI extends PApplet {
         textAlign(CENTER, CENTER);
         
 	}
+
+
+	// 
+	public void loadFile() {
+		String[] stringArray = this.loadStrings("small.txt");
+
+		for(int stringIndex = 0; stringIndex < stringArray.length; stringIndex++) {
+			String[] arrayWords = split(stringArray[stringIndex], ' '); // Split a string into an array of words
+			
+			for(int wordIndex = 0; wordIndex < arrayWords.length; wordIndex++) {
+				arrayWords[wordIndex].replaceAll("[^\\w\\s]","");
+				arrayWords[wordIndex].toLowerCase();
+			} // End for
+		} // End for
+
+
+
+		
+		w.replaceAll("[^\\w\\s]",""); // Remove punction characters
+		s.toLowerCase() // Convert a string to lower case 
+	} // End void loadFile
+
+	public void findWord(String string) {
+
+	} // End void findWord()
 }
--- java/src/ie/tudublin/Follow.java
+++ java/src/ie/tudublin/Follow.java
@@ -1,5 +1,31 @@
 package ie.tudublin;
 
 public class Follow {
+    // Private feilds
+    String word;
+    int count;
+
+    public Follow(String word, int count) {
+        this.word = word;
+        this.count = count;
+    } // End Follow Constructor
+
+
+
+
+    // Accessort Methods
+    public String getWord() {
+        return word;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String toString() {
+        return "Word: " + word + "Appears " + count + "times.";
+    }   
+
+
     
-}
+} // End class Follow
